f4db175ca84f0132139353ca6f47ea97d430b76e
Monica Smith <monica.smith463@gmail.com>
2025-10-24 10:39:00 -0700

    isLoggedIn hook: fix hydration Window undefined for SSR

--- frontend/src/app/(study-assistant)/upload/page.tsx
+++ frontend/src/app/(study-assistant)/upload/page.tsx
@@ -1,12 +1,19 @@
-
+import { redirect } from "next/navigation"; // ✅ use redirect
 import DocumentDropzoneComponent from "@/components/form/form-elements/DocumentDropZone";
 import { Metadata } from "next";
+import { cookies } from "next/headers";
 
 export const metadata: Metadata = {
   title: "Upload Page",
   description: "This is Next.js Upload Page",
 };
 
-export default function Upload() {
+export default async function Upload() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("access_token")?.value;
+
+  if (!token) {
+    redirect("/error-404");
+  }
   return <DocumentDropzoneComponent />;
 }
