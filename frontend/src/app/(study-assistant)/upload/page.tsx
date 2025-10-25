e410916b616ac1a5e404f1891d4eb598529fcd89
Monica Smith <monica.smith463@gmail.com>
2025-10-25 10:59:24 -0700

    something going wrong with network error

--- frontend/src/app/(study-assistant)/upload/page.tsx
+++ frontend/src/app/(study-assistant)/upload/page.tsx
@@ -1,7 +1,5 @@
-import { redirect } from "next/navigation"; // ✅ use redirect
 import DocumentDropzoneComponent from "@/components/form/form-elements/DocumentDropZone";
 import { Metadata } from "next";
-import { cookies } from "next/headers";
 
 export const metadata: Metadata = {
   title: "Upload Page",
@@ -9,11 +7,5 @@ export const metadata: Metadata = {
 };
 
 export default async function Upload() {
-  const cookieStore = await cookies();
-  const token = cookieStore.get("access_token")?.value;
-
-  if (!token) {
-    redirect("/error-404");
-  }
   return <DocumentDropzoneComponent />;
 }
--- frontend/src/hooks/document.ts
+++ frontend/src/hooks/document.ts
@@ -1,9 +1,10 @@
-import { useMutation } from '@tanstack/react-query';
-import { DocumentsCreateDocumentData, DocumentsService } from '@/client';
+import { useMutation } from "@tanstack/react-query";
+import { DocumentsService } from "@/client";
 
 export function useCreateDocument() {
   return useMutation({
-    mutationFn: (data: DocumentsCreateDocumentData) =>
-      DocumentsService.createDocument(data),
+    mutationFn: (formData: FormData) =>
+      // @ts-ignore: DocumentsService expects JSON, but we need FormData
+      DocumentsService.createDocument(formData),
   });
 }
