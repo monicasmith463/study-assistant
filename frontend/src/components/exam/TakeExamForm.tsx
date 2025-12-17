b1f7b6e9e241064fb6bb57b2f3924627f235be98
Monica Smith <monica.smith463@gmail.com>
2025-12-17 11:10:50 -0800

    add api call to fetch exam by exam_id

--- frontend/src/components/exam/TakeExamForm.tsx
+++ frontend/src/components/exam/TakeExamForm.tsx
@@ -1,25 +1,80 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "next/navigation";
+
 import Button from "../ui/button/Button";
 import Form from "../form/Form";
 import ComponentCard from "../common/ComponentCard";
 import QuestionInput from "./QuestionInput";
+import { useMutation } from "@tanstack/react-query";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
 
 export default function TakeExamForm() {
+  const { examId } = useParams<{ examId: string }>();
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("Form submitted:");
+    console.log("Submitting exam:", examId);
   };
+
+
+
+  const getExamMutation = useMutation({
+      mutationFn: async (examId: string) => {
+        const token = localStorage.getItem("access_token");
+
+        return fetch(`${API_URL}/api/v1/exams/${examId}`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token || ""}`,
+          },
+        })
+          .then((res) => {
+            if (!res.ok) {
+              throw new Error(`Failed to fetch exam: ${res.status}`);
+            }
+            return res.json();
+          })
+          .catch((err) => {
+            console.error("Fetch exam error:", err);
+            throw err;
+          });
+      },
+
+      onSuccess: (data) => {
+        console.log("Exam fetched successfully!");
+        console.log(data);
+      },
+
+      onError: (err: unknown) => {
+        console.error("getExamMutation error:", err);
+      },
+    });
+
+    useEffect(() => {
+      if (examId) {
+        getExamMutation.mutate(examId);
+      }
+    }, [examId, getExamMutation]);
+
+  if (!examId) {
+    return <div>Invalid exam</div>;
+  }
+
+  if (getExamMutation.isError) {
+    return <div>Failed to load exam</div>;
+  }
+
+  // const exam = getExamMutation.data;
+
   return (
     <ComponentCard title="Exam Form">
       <Form onSubmit={handleSubmit}>
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
-
-
-        {Array.from({ length: 5 }).map((_, i) => (
-          <QuestionInput key={i} />
-        ))}
-
+          {Array.from({ length: 5 }).map((_, i) => (
+            <QuestionInput key={i} />
+          ))}
 
           <div className="col-span-full">
             <Button className="w-full" size="sm">
