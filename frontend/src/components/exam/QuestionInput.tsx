54f48f2b316f37c9d5f99a88212364c6dfe1e6d8
Monica Smith <monica.smith463@gmail.com>
2025-12-17 18:21:21 -0800

    delete QuestionInput

--- frontend/src/components/exam/QuestionInput.tsx
+++
@@ -1,9 +0,0 @@
-import ListWithRadio from "../ui/list/ListWithRadio";
-
-export default function QuestionInput() {
-  return (
-    <div className="col-span-full">
-
-      <ListWithRadio />
-    </div>)
-}
