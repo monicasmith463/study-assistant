a1c33ddf69e5aff781b12ccf9012498751f2c2af
Monica Smith <monica.smith463@gmail.com>
2025-12-19 13:43:22 -0800

    see if update to conftest.py on session.rollback() fixes build DB failing

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -2,38 +2,21 @@ from collections.abc import Generator
 
 import pytest
 from fastapi.testclient import TestClient
-from sqlmodel import Session, delete
+from sqlmodel import Session
 
 from app.core.config import settings
-from app.core.db import engine, init_db
-from app.main import app
-from app.models import Answer, Document, Exam, Question, User
+from app.core.db import engine
 from tests.utils.user import authentication_token_from_email
 from tests.utils.utils import get_superuser_token_headers
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="function")
 def db() -> Generator[Session, None, None]:
     with Session(engine) as session:
-        init_db(session)
-        yield session
-        statement = delete(Answer)
-        session.execute(statement)
-        statement = delete(Question)
-        session.execute(statement)
-        statement = delete(Exam)
-        session.execute(statement)
-        statement = delete(Document)
-        session.execute(statement)
-        statement = delete(User)
-        session.execute(statement)
-        session.commit()
-
-
-@pytest.fixture(scope="module")
-def client() -> Generator[TestClient, None, None]:
-    with TestClient(app) as c:
-        yield c
+        try:
+            yield session
+        finally:
+            session.rollback()
 
 
 @pytest.fixture(scope="module")
