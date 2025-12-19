f29afcdb325f1cc4f959c13da19142c5f2c30560
Monica Smith <monica.smith463@gmail.com>
2025-12-18 16:16:06 -0800

    don't prerender on a dynamic route

--- frontend/src/app/(study-assistant)/take-exam/page.tsx
+++ frontend/src/app/(study-assistant)/take-exam/page.tsx
@@ -1,6 +1,9 @@
 import TakeExamForm from "@/components/exam/TakeExamForm";
 import { Metadata } from "next";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export const metadata: Metadata = {
   title: "Next.js TakeExam Page",
   description: "This is Next.js TakeExam Page",
